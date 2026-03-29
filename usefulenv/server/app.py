2a15fccf407880015b5ddf77b23edc7b2f8cccf0
SPARSH GUPTA <sgupta13_be23@thapar.edu>
2026-03-29 18:16:17 +0530

    Update app.py

--- usefulenv/server/app.py
+++ usefulenv/server/app.py
@@ -26,3 +26,10 @@ def step(action: dict):
         "reward": result.reward,
         "done": result.done,
     }
+def main():
+    import uvicorn
+    uvicorn.run("server.app:app", host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    main()
